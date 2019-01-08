3207d00aad0414ff16553882a035c0ca48a17e7f
gadamer1 <dngmlwns21@korea.ac.kr>
2019-01-08 19:48:50 +0900

    Tutorial2/

---
+++ Tutorial2/app/src/main/java/com/example/android/tutorial2/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.android.tutorial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void addclick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2= (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1+n2));
+    }
+    public void minusclick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2= (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1-n2));
+    }
+    public void multiplyclick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2= (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1*n2));
+    } public void divideclick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2= (EditText) findViewById(R.id.number2);
+        TextView result = (TextView) findViewById(R.id.result);
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+        result.setText(Integer.toString(n1/n2));
+    }
+
+
+
+
+}
